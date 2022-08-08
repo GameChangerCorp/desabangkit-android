332f6339d2d20e14e4492c428a7daa3bdc3bd9db
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-08-08 19:02:58 +0700

    - remove api client service

--- app/src/main/java/id/buildindo/desabangkit/android/core/data/remote/api/ApiConfig.kt
+++ app/src/main/java/id/buildindo/desabangkit/android/core/data/remote/api/ApiConfig.kt
@@ -3,37 +3,18 @@ package id.buildindo.desabangkit.android.core.data.remote.api
 import com.chuckerteam.chucker.api.ChuckerCollector
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.chuckerteam.chucker.api.RetentionManager
+import id.buildindo.desabangkit.android.MyApplication
 import id.buildindo.desabangkit.android.ui.pages.SplashScreenActivity
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object ApiConfig {
-
-    private val chuckerCollector = ChuckerCollector(
-        context = SplashScreenActivity.appContext,
+class ApiConfig {
+    val chuckerCollector = ChuckerCollector(
+        context = MyApplication.appContext,
         showNotification = true,
         retentionPeriod = RetentionManager.Period.ONE_HOUR
     )
-
-    // Create the Interceptor
-    private val chuckerInterceptor = ChuckerInterceptor.Builder(SplashScreenActivity.appContext)
-        .collector(chuckerCollector)
-        .maxContentLength(250_000L)
-        .alwaysReadResponseBody(true)
-        .build()
-
-    private val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-    private val client = OkHttpClient.Builder()
-        .addInterceptor(chuckerInterceptor)
-        .addInterceptor(loggingInterceptor)
-        .build()
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("http://api-desabangkit.herokuapp.com/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(client)
-        .build()
-    val apiInstance: ApiService = retrofit.create(ApiService::class.java)
 }
 
