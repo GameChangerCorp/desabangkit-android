0ad555d986b29fdeed60d93ce7c07153a9882e27
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-08-14 03:50:33 +0700

    - remove unused fragment

--- app/src/main/java/id/buildindo/desabangkit/android/ui/pages/onboarding/OnboardingFragment.kt
+++ app/src/main/java/id/buildindo/desabangkit/android/ui/pages/onboarding/OnboardingFragment.kt
@@ -1,27 +1,59 @@
 package id.buildindo.desabangkit.android.ui.pages.onboarding
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import id.buildindo.desabangkit.android.R
+import id.buildindo.desabangkit.android.core.data.local.datastore.DataStorePreference
 import id.buildindo.desabangkit.android.databinding.FragmentOnboardingBinding
 import id.buildindo.desabangkit.android.ui.adapter.OnBoardingAdapter
+import id.buildindo.desabangkit.android.ui.pages.LoginActivity
+import id.buildindo.desabangkit.android.ui.viewmodel.DatastoreViewModel
+import id.buildindo.desabangkit.android.ui.viewmodel.PreferenceViewModelFactory
+import timber.log.Timber
 
 
-class OnboardingFragment : Fragment() {
+class OnBoardingFragment : Fragment() {
 
-    private var _binding: FragmentOnboardingBinding? = null
-    private val binding get() = _binding!!
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+    private lateinit var _viewModelDataStore: DatastoreViewModel
+
+    private lateinit var _binding: FragmentOnboardingBinding
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity?.onBackPressedDispatcher?.addCallback(
+            this, // lifecycle owner
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    // Back is pressed... Finishing the activity
+                    activity?.finish()
+                }
+            })
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
-        return binding.root
+        initializeViewModel()
+        return _binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -39,7 +71,55 @@ class OnboardingFragment : Fragment() {
             lifecycle
         )
 
-        _binding?.viewPager2?.adapter = adapter
+        _binding.apply {
+            viewPager2.adapter = adapter
+
+            TabLayoutMediator(tabLayout, viewPager2) { _, _ ->
+
+            }.attach()
+        }
+
+        initializeViewPager()
+
+    }
+
+    private fun initializeViewPager() {
+        with(_binding) {
+            viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    if (position == 2) {
+                        Timber.d("cek status onboard")
+                        btnNext.text = getString(R.string.end_title)
+                        btnNext.setOnClickListener {
+                            _viewModelDataStore.saveOnBoardingState(true)
+                            moveToLoginActivity()
+                        }
+                    } else {
+                        btnNext.text = getString(R.string.next)
+                        btnNext.setOnClickListener {
+                            viewPager2.currentItem += 1
+                        }
+                    }
+                    super.onPageSelected(position)
+                }
+            })
+
+            Timber.d("cek status ${viewPager2.currentItem}")
+        }
+    }
+
+    private fun moveToLoginActivity() {
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        startActivity(intent)
+        activity?.finish()
+    }
+
+    private fun initializeViewModel() {
+        val pref = DataStorePreference.getInstance(requireContext().dataStore)
+        _viewModelDataStore = ViewModelProvider(
+            this,
+            PreferenceViewModelFactory(pref)
+        )[DatastoreViewModel::class.java]
 
     }
 
