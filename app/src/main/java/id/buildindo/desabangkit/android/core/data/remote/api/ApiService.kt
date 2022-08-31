a17fee139a99fefe336239d7a877d11e6818f027
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-09-01 02:37:00 +0700

    ! update apiservice api bangkit

--- app/src/main/java/id/buildindo/desabangkit/android/core/data/remote/api/ApiService.kt
+++ app/src/main/java/id/buildindo/desabangkit/android/core/data/remote/api/ApiService.kt
@@ -1,16 +1,20 @@
 package id.buildindo.desabangkit.android.core.data.remote.api
 
 import id.buildindo.desabangkit.android.core.data.remote.response.AddPhotoResponse
+import id.buildindo.desabangkit.android.core.data.remote.response.CodeVerificationRequest
+import id.buildindo.desabangkit.android.core.data.remote.response.CodeVerificationResponse
 import id.buildindo.desabangkit.android.core.data.remote.response.login.LoginRequest
 import id.buildindo.desabangkit.android.core.data.remote.response.login.LoginResponse
+import id.buildindo.desabangkit.android.core.data.remote.response.products.PostProductRequest
+import id.buildindo.desabangkit.android.core.data.remote.response.products.PostProductResponse
+import id.buildindo.desabangkit.android.core.data.remote.response.products.UploadPhotoResponse
 import id.buildindo.desabangkit.android.core.data.remote.response.register.RegisterRequest
 import id.buildindo.desabangkit.android.core.data.remote.response.register.RegisterResponse
+import id.buildindo.desabangkit.android.core.data.remote.response.roles.RolesResponse
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
+import retrofit2.http.*
 
 interface ApiService {
 
@@ -24,9 +28,28 @@ interface ApiService {
         @Body body: LoginRequest
     ) : Response<LoginResponse>
 
+    @GET("/users/role")
+    suspend fun getUserRole() : Response<RolesResponse>
+
+    @POST("/users/verification-account")
+    suspend fun sendUserVerification(
+        @Query("code") code : String? = null
+    ) : Response<CodeVerificationResponse>
+
+    @PUT("/users/send-verification")
+    suspend fun resendVerification(
+        @Query("email") email : String? = null
+    ) : Response<CodeVerificationResponse>
+
+    @POST("/users/products")
+    suspend fun postProducts(
+        @Query("preorder") preorder: Boolean? = null,
+        @Body body: PostProductRequest
+    ): Response<PostProductResponse>
+
     @Multipart
     @POST("/upload")
-    suspend fun checkUploadPhoto(
-        @Part file : MultipartBody.Part
-    ): Response<AddPhotoResponse>
+    suspend fun uploadPhoto(
+        @Part file : MultipartBody.Part,
+    ): Response<UploadPhotoResponse>
 }
