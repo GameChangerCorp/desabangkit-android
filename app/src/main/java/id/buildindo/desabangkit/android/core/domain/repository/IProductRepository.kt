27a434a884a2252bf684426755db84b922374ff2
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-09-01 02:43:38 +0700

    ! update new interface repository

---
+++ app/src/main/java/id/buildindo/desabangkit/android/core/domain/repository/IProductRepository.kt
@@ -0,0 +1,18 @@
+package id.buildindo.desabangkit.android.core.domain.repository
+
+import id.buildindo.desabangkit.android.core.data.remote.response.products.PostProductRequest
+import id.buildindo.desabangkit.android.core.data.remote.response.products.PostProductResponse
+import id.buildindo.desabangkit.android.core.data.remote.response.products.UploadPhotoResponse
+import id.buildindo.desabangkit.android.core.data.remote.response.products.category.CategoryResponse
+import id.buildindo.desabangkit.android.core.data.remote.response.products.products.ProductsResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
+
+interface IProductRepository {
+    suspend fun getAllProduct() : Response<ProductsResponse>
+    suspend fun getCategories() : Response<CategoryResponse>
+    suspend fun getProductByCategoryId(id: String) : Response<ProductsResponse>
+    suspend fun postProduct(preorder: Boolean? = null, body: PostProductRequest) : Response<PostProductResponse>
+    suspend fun uploadPhoto(photo: MultipartBody.Part) : Response<UploadPhotoResponse>
+}
