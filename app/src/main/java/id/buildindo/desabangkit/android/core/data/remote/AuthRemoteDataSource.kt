78fa849b5b2b76e69c99fb489bc0dd01517f6f8d
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-09-01 02:37:18 +0700

    ! update auth data source

--- app/src/main/java/id/buildindo/desabangkit/android/core/data/remote/AuthRemoteDataSource.kt
+++ app/src/main/java/id/buildindo/desabangkit/android/core/data/remote/AuthRemoteDataSource.kt
@@ -1,10 +1,13 @@
 package id.buildindo.desabangkit.android.core.data.remote
 
 import id.buildindo.desabangkit.android.core.data.remote.api.ApiService
+import id.buildindo.desabangkit.android.core.data.remote.response.CodeVerificationRequest
+import id.buildindo.desabangkit.android.core.data.remote.response.CodeVerificationResponse
 import id.buildindo.desabangkit.android.core.data.remote.response.login.LoginRequest
 import id.buildindo.desabangkit.android.core.data.remote.response.login.LoginResponse
 import id.buildindo.desabangkit.android.core.data.remote.response.register.RegisterRequest
 import id.buildindo.desabangkit.android.core.data.remote.response.register.RegisterResponse
+import id.buildindo.desabangkit.android.core.data.remote.response.roles.RolesResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Response
@@ -12,14 +15,30 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AuthRemoteDataSource @Inject constructor(private val service: ApiService) : IAuthRemoteDataSource {
+class AuthRemoteDataSource @Inject constructor(private val service: ApiService) :
+    IAuthRemoteDataSource {
     override suspend fun registerUser(body: RegisterRequest): Response<RegisterResponse> =
-      withContext(Dispatchers.IO) {
-          service.registerUser(body)
-      }
+        withContext(Dispatchers.IO) {
+            service.registerUser(body)
+        }
 
     override suspend fun loginUser(body: LoginRequest): Response<LoginResponse> =
         withContext(Dispatchers.IO) {
             service.loginUser(body)
+        }
+
+    override suspend fun rolesUser(): Response<RolesResponse> =
+        withContext(Dispatchers.IO) {
+            service.getUserRole()
+        }
+
+    override suspend fun sendUserVerification(code: String): Response<CodeVerificationResponse> =
+        withContext(Dispatchers.IO) {
+            service.sendUserVerification(code)
+        }
+
+    override suspend fun resendUserVerification(email: String): Response<CodeVerificationResponse> =
+        withContext(Dispatchers.IO) {
+            service.resendVerification(email)
         }
 }
--- app/src/main/java/id/buildindo/desabangkit/android/core/data/remote/IAuthRemoteDataSource.kt
+++ app/src/main/java/id/buildindo/desabangkit/android/core/data/remote/IAuthRemoteDataSource.kt
@@ -1,12 +1,17 @@
 package id.buildindo.desabangkit.android.core.data.remote
 
+import id.buildindo.desabangkit.android.core.data.remote.response.CodeVerificationResponse
 import id.buildindo.desabangkit.android.core.data.remote.response.login.LoginRequest
 import id.buildindo.desabangkit.android.core.data.remote.response.login.LoginResponse
 import id.buildindo.desabangkit.android.core.data.remote.response.register.RegisterRequest
 import id.buildindo.desabangkit.android.core.data.remote.response.register.RegisterResponse
+import id.buildindo.desabangkit.android.core.data.remote.response.roles.RolesResponse
 import retrofit2.Response
 
 interface IAuthRemoteDataSource {
     suspend fun registerUser(body: RegisterRequest) : Response<RegisterResponse>
     suspend fun loginUser(body: LoginRequest) : Response<LoginResponse>
+    suspend fun rolesUser() : Response<RolesResponse>
+    suspend fun sendUserVerification(code : String) : Response<CodeVerificationResponse>
+    suspend fun resendUserVerification(email : String) : Response<CodeVerificationResponse>
 }
